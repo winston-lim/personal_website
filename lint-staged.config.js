a3d0b88f1febbd473c4d140ab59777d99074d49e
winston-lim <winston_lim1@hotmail.com>
2022-06-09 00:00:09 +0800

    Update lint-staged config as per NextJS documentations

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,17 @@
+// module.exports = {
+//   // Run type-check on changes to TypeScript files
+//   '**/*.ts?(x)': () => 'yarn type-check',
+//   // Run ESLint on changes to JavaScript/TypeScript files
+//   '**/*.(ts|js)?(x)': (filenames) => `yarn lint . ${filenames.join(' ')}`,
+// }
+const path = require('path')
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`
+
 module.exports = {
-  // Run type-check on changes to TypeScript files
   '**/*.ts?(x)': () => 'yarn type-check',
-  // Run ESLint on changes to JavaScript/TypeScript files
-  '**/*.(ts|js)?(x)': (filenames) => `yarn lint . ${filenames.join(' ')}`,
-} 
+  '**/*.(ts|js)?(x)': [buildEslintCommand],
+}
