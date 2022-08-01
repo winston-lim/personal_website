d678839c4449afe5059fe3acef26b7e90c23ef0b
winston-lim <winston_lim1@hotmail.com>
2022-08-01 22:03:59 +0800

    Update: _app to use colorModeManager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import { Chakra } from '../components/ChakraWrapper'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <Chakra cookies={pageProps.cookies}>
       <Component {...pageProps} />
-    </ChakraProvider>
+    </Chakra>
   )
 }
 
