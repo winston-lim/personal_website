9e218cb9bf5f01f01de6449703cf5962c7cb7cb2
winston-lim <winston_lim1@hotmail.com>
2022-08-04 18:18:25 +0800

    Update elsint config

---
+++ pages/maintenance.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+type Props = {}
+
+const MaintenancePage = ({}: Props) => {
+  return (
+    <div
+      style={{
+        width: '100%',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      Currently under maintenance
+    </div>
+  )
+}
+
+export default MaintenancePage
