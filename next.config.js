abfdcedd49526711bbaeaa6631b572c49da36fc0
winston-lim <winston_lim1@hotmail.com>
2022-08-04 18:21:24 +0800

    Update: add maintenance page and redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    if (process.env.MAINTENANCE === 'true') {
+      return [
+        {
+          source: '/((?!maintenance).*)',
+          destination: '/maintenance',
+          permanent: false,
+        },
+      ]
+    }
+    return []
+  },
   reactStrictMode: true,
+  env: {
+    ENV: process.env.ENV,
+    MAINTENANCE: process.env.MAINTENANCE,
+  },
 }
 
 module.exports = nextConfig
