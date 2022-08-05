711e151b5c7bf3cbbdbf88c1eccec80b98768feb
winston-lim <winston_lim1@hotmail.com>
2022-08-05 22:13:07 +0800

    Add theme colors

--- theme.ts
+++ theme.ts
@@ -1,31 +1,19 @@
 import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
 
-// const themeConfig: ThemeConfig = {
-//   initialColorMode: 'dark',
-//   useSystemColorMode: false,
-// }
-
-// export const theme = extendTheme({
-//   config: { themeConfig },
-//   colors: {
-//     transparent: 'transparent',
-//     black: '#000',
-//     white: '#fff',
-//     gray: {
-//       50: '#f7fafc',
-//       // ...
-//       900: '#171923',
-//     },
-//     // ...
-//   },
-// })
-
 const config = {
   initialColorMode: 'dark',
   useSystemColorMode: false,
 }
 
 // 3. extend the theme
-const theme = extendTheme({ config })
+const theme = extendTheme({
+  config,
+  colors: {
+    transparent: 'transparent',
+    primaryColor: '#92DCE5',
+    accentColor: '#FCDDBC',
+    backgroundColor1: '#494947',
+  },
+})
 
 export default theme
