ea6a679ba73c02478149f648ea51afefbf90525c
winston-lim <winston_lim1@hotmail.com>
2022-11-16 18:03:55 +0800

    Add support for vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import 'styles/globals.scss'
+import { Analytics } from '@vercel/analytics/react'
 import type { AppProps } from 'next/app'
 import { Chakra } from 'components/Chakra/Chakra'
 
@@ -6,6 +7,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Chakra cookies={pageProps.cookies}>
       <Component {...pageProps} />
+      <Analytics />
     </Chakra>
   )
 }
